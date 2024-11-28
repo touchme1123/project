a6231594e4629360fc7f8fcca91d3b6788167d9b
touchme1123 <touchme11309@gmail.com>
2024-11-28 21:17:02 +0900

    FE SETTING

--- demo/src/main/java/com/example/demo/domain/MemberRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.domain;
-
-public enum MemberRole {
-
-    USER,MANAGER,ADMIN;
-}
--- demo/src/main/java/com/example/demo/search/ProjectSearch.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.search;
-
-import com.example.demo.dto.PageRequestDTO;
-import com.example.demo.dto.PageResponseDTO;
-import com.example.demo.dto.ProjectDTO;
-
-public interface ProjectSearch {
-    PageResponseDTO<ProjectDTO> searchList (PageRequestDTO pageRequestDTO);
-}
